9beccd9fadddc0353b8cd838aa4dbe768c244c32
Shaunak Das <shaunakdas2020@gmail.com>
2017-10-23 08:54:05 +0530

    made changes to add random text in category string

--- spec/factories/standards.rb
+++ spec/factories/standards.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :standard do
-    # name {'Category '+[*('A'..'Z')].sample(8).join}
-    sequence(:name) { |n| "Category #{n}" }
-    # slug {'category_'+[*('A'..'Z')].sample(8).join}
-    sequence(:slug) { |n| "category_#{n}" }
-    add_attribute :sequence, Faker::Number.between(1, 200)
+    name {'Category '+[*('A'..'Z')].sample(8).join}
+    # sequence(:name) { |n| "Category #{n}" }
+    slug {'category_'+[*('A'..'Z')].sample(8).join}
+    # sequence(:slug) { |n| "category_#{n}" }
+    add_attribute :sequence, Random.rand(200)
   end
 
 end
