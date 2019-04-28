65b51dfa024e6482e50f11c19e14cfd64f956811
Shaunakdas <shaunak.das@tokopedia.com>
2019-04-28 20:01:11 +0530

    Added score_algo in game_holder detail serializer

--- app/serializers/api/v1/game_holder_detail_serializer.rb
+++ app/serializers/api/v1/game_holder_detail_serializer.rb
@@ -1,5 +1,5 @@
 module Api::V1
   class GameHolderDetailSerializer < GameHolderSerializer
-    attributes  :benefits, :score, :sub_title, :question_input
+    attributes  :benefits, :score, :sub_title, :question_input, :score_algo
   end
 end
