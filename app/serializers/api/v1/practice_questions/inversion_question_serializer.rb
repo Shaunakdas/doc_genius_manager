dcb516d15924e81bec200e78ff4901f55bbd5f38
Shaunakdas <shaunak.das@tokopedia.com>
2019-05-20 22:12:13 +0530

    Added inversion serializer

--- app/serializers/api/v1/practice_questions/inversion_question_serializer.rb
+++ app/serializers/api/v1/practice_questions/inversion_question_serializer.rb
@@ -1,9 +1,14 @@
 module Api::V1::PracticeQuestions
   class InversionQuestionSerializer < PracticeQuestionSerializer
-    attributes :id, :answer, :options
+    attributes :id, :hint, :options
 
     def options
-      ActiveModel::ArraySerializer.new(object.game_options, each_serializer: InversionOptionSerializer)
+      object.game_options.map{|x|x.option.display}
+      # ActiveModel::ArraySerializer.new(object.game_options, each_serializer: InversionOptionSerializer)
+    end
+
+    def hint
+      object.question.solution
     end
   end
 end
