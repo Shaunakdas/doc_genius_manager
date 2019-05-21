63823975e62adee29289a80d7e8184e4bdf4f6bc
Shaunakdas <shaunak.das@tokopedia.com>
2019-05-21 18:50:32 +0530

    Added inversion game question field

--- app/serializers/api/v1/practice_questions/inversion_game_serializer.rb
+++ app/serializers/api/v1/practice_questions/inversion_game_serializer.rb
@@ -1,6 +1,10 @@
 module Api::V1::PracticeQuestions
   class InversionGameSerializer < PracticeGameSerializer
-    attributes :title, :time, :entry_sequence, :entry_delay, :pairs
+    attributes :title, :time, :entry_sequence, :entry_delay, :question,  :pairs
+
+    def question
+      object.game_questions[0].question.display
+    end
 
     def pairs
       ActiveModel::ArraySerializer.new(object.game_questions.first.sub_questions, each_serializer: InversionQuestionSerializer)
