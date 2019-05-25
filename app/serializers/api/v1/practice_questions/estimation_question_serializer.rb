ccd3a66c621e0c7c90dad49af47285dc6202cc67
Shaunakdas <shaunak.das@tokopedia.com>
2019-05-25 21:00:13 +0530

    Added number_line key in estimation json

--- app/serializers/api/v1/practice_questions/estimation_question_serializer.rb
+++ app/serializers/api/v1/practice_questions/estimation_question_serializer.rb
@@ -1,9 +1,9 @@
 module Api::V1::PracticeQuestions
   class EstimationQuestionSerializer < PracticeQuestionSerializer
-    attributes :id, :question, :tips, :hint, :mode, :marker_gap, :options, :answer
+    attributes :id, :question, :tips, :hint, :number_line
 
     def options
-      ActiveModel::ArraySerializer.new(object.incorrect_game_options, each_serializer: EstimationOptionSerializer)
+      
     end
 
     def hint
@@ -12,6 +12,15 @@ module Api::V1::PracticeQuestions
 
     def answer
       EstimationAnswerSerializer.new(object.correct_game_options[0]).as_json[:estimation_answer]
+    end
+
+    def number_line
+      {
+        mode: object.question.mode,
+        marker_gaps:  MarkerGapSerializer.new(object.question.marker_gap).as_json[:marker_gap],
+        display: ActiveModel::ArraySerializer.new(object.incorrect_game_options, each_serializer: EstimationOptionSerializer),
+        answer: EstimationAnswerSerializer.new(object.correct_game_options[0]).as_json[:estimation_answer]
+      }
     end
   end
 end
