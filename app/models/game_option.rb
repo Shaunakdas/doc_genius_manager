e45e501f8f3ed2d76cc7f6a99286ada2521b5e2d
Shaunakdas <shaunak.das@tokopedia.com>
2019-07-22 07:26:07 +0530

    Added fraction options in dragonbox serializers

--- app/models/game_option.rb
+++ app/models/game_option.rb
@@ -1,9 +1,10 @@
 class GameOption < ApplicationRecord
-  belongs_to :option
-  belongs_to :game_question
+  belongs_to :option, optional: true
+  belongs_to :game_question, optional: true
   has_many :game_option_attempts
-  has_many :sub_options, class_name: "Option", foreign_key: "parent_option_id"
-  belongs_to :parent_option, class_name: "Option", optional: true
+  has_many :sub_options, class_name: "GameOption", foreign_key: "parent_option_id"
+  belongs_to :parent_option, class_name: "GameOption", optional: true
+  belongs_to :option_type, optional: true
   enum position: [ :numerator, :denominator ]
 
   def create_attempt_data option_obj, game_question_attempt
--- app/serializers/api/v1/practice_questions/dragonbox_question_serializer.rb
+++ app/serializers/api/v1/practice_questions/dragonbox_question_serializer.rb
@@ -13,23 +13,43 @@ module Api::V1::PracticeQuestions
 
     def game_section position
       object.sub_questions.each do |game_ques|
-        pp position
-        pp game_ques.question.position
-        puts game_ques.question.position == position
         return game_ques if game_ques.question.position.to_s == position.to_s
+      end
+      return nil
+    end
+
+    def items section, position
+      fraction_op_type = OptionType.where(slug: "fraction").first
+      if section && fraction_op_type
+        return section.game_options.where(option_type: [nil]).map{ |r| r.option.reference_id}
+      end
+      return nil
+    end
+
+    def fraction_items section, position
+      fraction_op_type = OptionType.where(slug: "fraction").first
+      if section && fraction_op_type
+        fraction_ops = section.game_options.where(option_type: fraction_op_type)
+        fraction_items = []
+        fraction_ops.last(1).each do |op|
+          fraction_items << { 
+            upper: op.sub_options.where(position: :numerator).map{ |r| r.option.reference_id},
+            lower: op.sub_options.where(position: :denominator).map{ |r| r.option.reference_id}
+          }
+        end
+        return fraction_items if fraction_items.length > 0
       end
       return nil
     end
 
     def op_items position
       section  = game_section(position)
-      return {
-        items: section.nil? ? nil : section.game_options.map{ |r| r.option.reference_id}
-      }
-    end
-
-    def hint
-      
+      items_obj = items(section, position)
+      fraction_items_obj = fraction_items(section, position)
+      op_items_obj = {}
+      op_items_obj[:items] = items_obj if (items_obj &&  items_obj.length > 0)
+      op_items_obj[:fractions] = fraction_items_obj if (fraction_items_obj &&  fraction_items_obj.length > 0)
+      return op_items_obj
     end
   end
 end
