bae8c4186a782970157ac2c174d13c0377725462
Shaunakdas <shaunak.das@tokopedia.com>
2020-03-25 19:12:12 +0530

    Added comment_time and display_style

--- app/serializers/api/v1/character_dialog_serializer.rb
+++ app/serializers/api/v1/character_dialog_serializer.rb
@@ -1,9 +1,12 @@
 module Api::V1
   class CharacterDialogSerializer < ActiveModel::Serializer
     attributes :id, :character, :left_weapon_colour, :right_weapon,
-      :right_weapon_colour, :count, :comment,  :position, :animation, :repeat_mode, :sequence 
+      :right_weapon_colour, :count, :comment, :comment_time,  :position, :animation, :repeat_mode, :sequence 
     has_one :left_weapon, serializer: WeaponSerializer
     has_one :right_weapon, serializer: WeaponSerializer
     has_one :character, serializer: CharacterSerializer
+    def comment_time
+      return object.comment.split(/\s+/).length/2 if !object.comment.nil?
+    end
   end
 end
--- app/serializers/api/v1/character_discussion_serializer.rb
+++ app/serializers/api/v1/character_discussion_serializer.rb
@@ -1,10 +1,15 @@
 module Api::V1
   class CharacterDiscussionSerializer < ActiveModel::Serializer
-    attributes :id, :name
+    attributes :id, :name, :display_style
     has_many :character_dialogs, serializer: CharacterDialogSerializer
     def character_dialogs
       return object.character_dialogs if object.character_dialogs.count > 0
       [CharacterDialog.get_default]
+    end
+
+    def display_style
+      return "text" if object.id%2 == 0
+      return "no_text" if object.id%2 == 1
     end
   end
 end
