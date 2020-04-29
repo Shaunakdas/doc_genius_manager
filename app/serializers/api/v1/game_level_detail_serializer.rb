a0cac0d6b8e8bbda01eeb81db0b93658c79ad3e2
Shaunakdas <shaunak.das@tokopedia.com>
2020-04-29 10:16:20 +0530

    Added extra check on game level victory cards

--- app/serializers/api/v1/game_level_detail_serializer.rb
+++ app/serializers/api/v1/game_level_detail_serializer.rb
@@ -2,17 +2,23 @@ module Api::V1
   class GameLevelDetailSerializer < GameLevelSerializer
     attributes  :chapter_sequence, :background_area ,:benefits, :score, :sub_title, :question_input, :score_algo,
       :practice_mode, :nature_effect, :sequence, :intro_character_discussion, :success_character_discussion,
-      :fail_character_discussion, :success_victory_cards
+      :fail_character_discussion, :final_victory_cards
     has_one :intro_character_discussion, serializer: CharacterDiscussionSerializer
     has_one :success_character_discussion, serializer: CharacterDiscussionSerializer
     has_one :fail_character_discussion, serializer: CharacterDiscussionSerializer
-    has_many :success_victory_cards, serializer: GameLevelVictoryCardSerializer
+    # has_many :success_victory_cards, serializer: GameLevelVictoryCardSerializer
     def chapter_sequence
       object.id
     end
 
     def background_area
       object.background_area
+    end
+
+    def final_victory_cards
+      return [] if object.success_victory_cards.length == 0
+      return [] if object.success_victory_cards[0].class.name != "GameLevelVictoryCard"
+      return ActiveModel::ArraySerializer.new(object.success_victory_cards, each_serializer: GameLevelVictoryCardSerializer)
     end
   end
 end
