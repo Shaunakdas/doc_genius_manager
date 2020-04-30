9403e82739914a129a6fe5d0760478458dc28c9f
Shaunakdas <shaunak.das@tokopedia.com>
2020-04-30 23:51:43 +0530

    Added star list in level map

--- app/serializers/api/v1/level_map_serializer.rb
+++ app/serializers/api/v1/level_map_serializer.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class LevelMapSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :email, :sex, :birth, :standard, :suggested_games, :chapters
+    attributes :id, :first_name, :last_name, :email, :sex, :birth, :standard, :suggested_games, :chapters, :star_list
     
     def sex
       object.sex.to_s.humanize if object.sex.present? 
@@ -16,6 +16,16 @@ module Api::V1
 
     def chapters
       ActiveModel::ArraySerializer.new(object.enabled_chapters, each_serializer: ChapterLevelSerializer)
+    end
+
+    def star_list
+      return nil if object.game_sessions.length == 0
+      attempt_list =  object.game_sessions.order(:created_at).uniq{|x| x.game_level_id}
+      star_counts = {}
+      attempt_list.each do |game_session|
+        star_counts[game_session.game_level_id]=game_session.attempt_score.star_count if !game_session.attempt_score.nil?
+      end
+      return star_counts
     end
     # has_many :recent_questions, serializer: QuestionTypeSerializer
   end
