d79a01099b9748bbcd0de7ed5dfab38004b6095f
Shaunakdas <shaunak.das@tokopedia.com>
2020-05-01 22:50:41 +0530

    Added game listing script

--- db/seeds/35_fetch_game_levels.rb
+++ db/seeds/35_fetch_game_levels.rb
@@ -1,5 +1,21 @@
-x = ""
-GameLevel.includes(:game_questions).where.not(game_questions: {id: nil}).each do |level|
-  x = x + "#{level.game_holder.game.slug},#{level.practice_mode},#{level.id},#{level.game_questions.count} \n"
+def get_game_types
+  x = ""
+  GameLevel.includes(:game_questions).where.not(game_questions: {id: nil}).each do |level|
+    x = x + "#{level.game_holder.game.slug},#{level.practice_mode},#{level.id},#{level.game_questions.count} \n"
+  end
+  puts x
 end
-puts x
+
+def get_level_list
+  y = ""
+  list = []
+  User.last.enabled_chapters.each do |c|
+    c.practice_game_levels.each do |level|
+      y = y + "#{c.sequence_standard},#{level.id},#{level.slug},#{level.practice_mode} \n"
+    end
+  end
+  puts y
+end
+
+# get_game_types
+# get_level_list
