ff1d56495e745bcc114f9b2531a0e3b48bdfbd27
Shaunakdas <shaunakdas2020@gmail.com>
2020-07-23 22:29:12 +0530

    Added character dialog default values

--- app/models/character_dialog.rb
+++ app/models/character_dialog.rb
@@ -16,6 +16,32 @@ class CharacterDialog < ApplicationRecord
     return character_dialog if character_dialog.save!
   end
 
+  def self.default_audio_map
+    {
+      "Yes Guru Drona, I am ready for my next challenge": "d1",
+      "Thank you Guru Drona": "d2",
+      "Sure, Guru Drona. I will replay this game and practice more.": "d3",
+      "Nicely Done Arjun!": "d4",
+      "You have learnt the skill but you did not keep the momentum. You need to practice more until you perfect this skill.": "d5",
+      "I am ready for the challenge.": "d6",
+      "I will not disappoint you Guru Drona again!": "d7",
+      "That was not a good performance. You need to practice lot more by retrying this challenge again and again.": "d8",
+      "Time for a competition between your brothers and cousins.": "d9",
+      "That was a wonderful performance": "d10",
+      "Thank you Guru Drona! I want to see your ranking among all your brothers and cousins.": "d11",
+      "You practised well but when you competed against your brothers, you lost the challenge!": "d12",
+      "Pressure of competing with my brothers took a toll on me.": "d13",
+      "Go son, practice more and more until you are ready for the ultimate challenge of competing with your brothers.": "d14",
+      "Sure, Guru Drona": "d15"
+    }
+  end
+
+  # def audio_url
+  #   audio_url = CharacterDialog.default_audio_map[comment]
+  #   return audio_url if !audio_url.nil?
+
+  # end
+
   def right_weapon_colours
     return [] if right_weapon_colour.nil?
     return right_weapon_colour.split(';')
@@ -54,5 +80,10 @@ class CharacterDialog < ApplicationRecord
   def boots_colours
     return [] if boots_colour.nil?
     return boots_colour.split(';')
+  end
+
+  def prefix_url
+    return nil if audio_url.nil?
+    return audio_url.chomp(".mp3")
   end
 end
