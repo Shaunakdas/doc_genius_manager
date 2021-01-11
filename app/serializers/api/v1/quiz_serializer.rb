7e0b09af684b3104f60a19e7f993c06141d18007
Shaunakdas <shaunak.das@tokopedia.com>
2021-01-11 14:48:55 +0530

    Added s3 link for external images

--- app/serializers/api/v1/quiz_serializer.rb
+++ app/serializers/api/v1/quiz_serializer.rb
@@ -1,6 +1,8 @@
 module Api::V1
   class QuizSerializer < AcadEntitySerializer
-    attributes :id, :title, :name, :slug, :image_url, :enabled
-
+    attributes :id, :title, :name, :slug, :s3_image_url, :enabled
+    def s3_image_url
+      "http://drona-player.docgenius.in/"+object.image_url.to_s
+    end
   end
 end
