b7d2f8e3c1eec72b6797c77b118274a536be2dcb
Shaunakdas <shaunak.das@tokopedia.com>
2021-01-30 19:45:55 +0530

    Added attribute enums for game holder session

--- app/models/game_holder_session.rb
+++ app/models/game_holder_session.rb
@@ -1,4 +1,6 @@
 class GameHolderSession < ApplicationRecord
   belongs_to :game_holder
   belongs_to :user
+  enum completion_status: [ :created, :started, :completed]
+  enum attempt_type: [ :homework, :permanent, :live]
 end
