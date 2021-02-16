12d29771a8a38a27bf23d0bfc8f288aef5f4d629
Shaunakdas <shaunak.das@tokopedia.com>
2021-02-16 07:53:59 +0530

    Added student score to reports details API

--- app/serializers/api/v1/game_session_serializer.rb
+++ app/serializers/api/v1/game_session_serializer.rb
@@ -1,11 +1,11 @@
 module Api::V1
   class GameSessionSerializer < ActiveModel::Serializer
-    attributes :id,:start, :finish, :recent_scores, :score_rank
+    attributes :id,:start, :finish, :recent_scores
     has_one :user, serializer: UserSerializer
 
     has_one :session_score, serializer: SessionScoreSerializer
 
-    has_many :recent_scores, serializer: SessionScoreSerializer
+    # has_many :recent_scores, serializer: SessionScoreSerializer
     # def question_type
     #   object.game_holder.question_type, serializer: QuestionTypeSerializer
     # end
--- app/serializers/api/v1/report_detail_serializer.rb
+++ app/serializers/api/v1/report_detail_serializer.rb
@@ -2,5 +2,6 @@ module Api::V1
   class ReportDetailSerializer < ActiveModel::Serializer
     attributes :id, :title, :completion_status, :attempt_type, :completion_date, :start_date
     has_one :game_holder, serializer: GameHolderDetailSerializer
+    has_many :game_sessions, serializer: GameSessionSerializer
   end
 end
