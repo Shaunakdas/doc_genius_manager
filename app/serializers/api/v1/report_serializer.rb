fa2f28274ed9593fd6b4bf7fa31c036eeb2c5e2e
Shaunakdas <shaunak.das@tokopedia.com>
2021-02-22 18:00:53 +0530

    Reports Listing changes done

--- app/serializers/api/v1/report_serializer.rb
+++ app/serializers/api/v1/report_serializer.rb
@@ -1,6 +1,14 @@
 module Api::V1
   class ReportSerializer < ActiveModel::Serializer
-    attributes :id, :title, :completion_status, :attempt_type, :completion_date, :start_date
-    has_one :game_holder, serializer: GameHolderSerializer
+    attributes :id, :title, :completion_status, :attempt_type, :completion_date, :start_date, :creation_date, :session_count
+    has_one :game_holder, serializer: GameHolderSerializer    
+    def creation_date
+      object.created_at.strftime("%d/%m/%y")
+    end
+
+    def session_count
+      object.game_sessions.length
+    end
+
   end
 end
