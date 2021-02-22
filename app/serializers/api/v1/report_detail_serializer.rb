c0c2a10665f4a47826aec9051d14403232ae05a3
Shaunakdas <shaunak.das@tokopedia.com>
2021-02-22 18:19:27 +0530

    Aded creation date in reports detailed

--- app/serializers/api/v1/report_detail_serializer.rb
+++ app/serializers/api/v1/report_detail_serializer.rb
@@ -1,7 +1,10 @@
 module Api::V1
   class ReportDetailSerializer < ActiveModel::Serializer
-    attributes :id, :title, :completion_status, :attempt_type, :completion_date, :start_date
+    attributes :id, :title, :completion_status, :attempt_type, :completion_date, :start_date, :creation_date
     has_one :game_holder, serializer: GameHolderDetailSerializer
     has_many :game_sessions, serializer: GameSessionSerializer
+    def creation_date
+      object.created_at.strftime("%d/%m/%y")
+    end
   end
 end
